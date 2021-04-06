e60812cd2ed1155dae7e484866c6b4e725c7f0ee
Mikayla <mikayla.orange@bison.howard.edu>
2021-04-06 12:04:27 -0400

    updates

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyB58whWDzklcWMUKLUjjnt7P4uYy0RgIDg",
+  authDomain: "fir-ex-6715a.firebaseapp.com",
+  databaseURL: "https://fir-ex-6715a.firebaseio.com",
+  projectId: "fir-ex-6715a",
+  storageBucket: "fir-ex-6715a.appspot.com",
+  messagingSenderId: "259027376708",
+  appId: "1:259027376708:web:b56631c1e58b6b6f8c7a11"
+};
+firebase.initializeApp(firebaseConfig);
